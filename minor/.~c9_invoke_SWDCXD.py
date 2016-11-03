8b3c28900730ddc516fb905767d3b47354a48c24
Rob <mailrobinfrancis@gmail.com>
2016-11-03 16:44:01 +0000

    fixing sms

--- minor/.~c9_invoke_SWDCXD.py
+++
@@ -1,6 +0,0 @@
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
-from django.views.generic import TemplateView
-def home(request):
-    return HttpResponseRedirect(reverse('auth:login'))
-    
